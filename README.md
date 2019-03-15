3e98d9c061413e01401d0d219d11db249d1e6b88
Mary Dang <dangmaryk@gmail.com>
2019-03-15 00:18:16 -0500

    finished display of images - still & animated

--- README.md
+++ README.md
@@ -46,11 +46,11 @@ Bootstrap Portfolio link: <https://mkd454.github.io/Bootstrap-Portfolio/>
 - [x] Your app should take the topics in this array and create buttons in your HTML.
    * Try using a loop that appends a button for each string in the array.
 
-- [ ] When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+- [x] When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 
-- [ ] When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+- [x] When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-- [ ] Under every gif, display its rating (PG, G, so on).
+- [x] Under every gif, display its rating (PG, G, so on).
    * This data is provided by the GIPHY API.
    * Only once you get images displaying with button presses should you move on to the next step.
 
--- javascript/functionality.js
+++ javascript/functionality.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
   //Topics String Array
   var topics = ["Meerkat","Hamster","Hedgehog","Mouse"];
 
+  //New input
+  $("#newTopicButton").on("click", function(e){
+    e.preventDefault();
+
+    var newTopic = $(this);
+    console.log(newTopic);
+  })
+
   //Create a button for every animal in the array
   function renderButtons(topics) {
     $("#buttonDiv").empty();
 
     for(var i=0;i<topics.length;i++) {
-      var button = $("<button value='"+topics[i]+"'>"+topics[i]+"</button>");
+      var button = $("<button type='button' class='btn btn-info m-1' value='"+topics[i]+"'>"+topics[i]+"</button>");
       $("#buttonDiv").append(button);
       button.addClass("giphyButton");
     }
@@ -29,21 +37,37 @@ $(document).ready(function() {
       method: "GET"
     }).then(function(response) {
       var results = response.data;
-      // console.log(results);
 
       for(var i=0;i<results.length;i++) {
         var rating = results[i].rating;
-        var imageURL = results[i].embed_url;
+        var stillImage = results[i].images.original_still.url;
+        var animateImage = results[i].images.original.url;
+        var state = "still";
 
         var newDiv = $("<div>");
         var p = $("<p>");
         p.text("Rating: " + rating);
         var image = $("<img>");
-        image.attr("src", imageURL);
+        image.attr("data-still",stillImage);
+        image.attr("data-animate",animateImage);
+        image.attr("data-state",state);
+        image.attr("src", stillImage);
+        image.addClass("gif");
 
         newDiv.append(p,image);
         $("#imageDiv").prepend(newDiv);
       }
+      $(document).on("click",".gif",function() {
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+          state = "animate";
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+          state = "still";
+        }
+      })
     })
   })
 });
